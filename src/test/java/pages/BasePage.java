65c02435ac373f082dc58276b5b250550b28c542
Aleksandra <taeyeononni@gmail.com>
2022-02-02 11:57:46 +0300

    add

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -5,10 +5,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 @Log4j2
 public abstract class BasePage {
     WebDriver driver;
     WebDriverWait wait;
+
+    public static final String BASE_URL = "https://www.fitday.com";
+    public static final By FORGOT_LINK = By.xpath("//p[@class='links']//a[1]");
+    public static final By CREATE_LINK = By.xpath("//p[@class='links']//a[2]");
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
