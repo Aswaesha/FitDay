e99bf4df7a3d52abfe542739558b07bcb7b384c8
Aleksandra <taeyeononni@gmail.com>
2022-02-02 12:06:37 +0300

    add

---
+++ src/test/java/pages/SingUpPage.java
@@ -0,0 +1,47 @@
+package pages;
+
+import elements.Input;
+import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
+import models.Account;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+@Log4j2
+public class SingUpPage extends BasePage {
+
+    public static final By ERROR_MESSAGE_PASSWORD = By.xpath("//p[@for='Password1']");
+    public static final By ERROR_MESSAGE_DIFFERENT_PASSWORD = By.xpath("//p[@for='Password2']");
+    public static final By ERROR_MESSAGE_INVALID_EMAIL = By.xpath("//p[@for='Email']");
+    public static final By ERROR_MESSAGE_NAME = By.xpath("//p[@for='Username']");
+    public static final By LOGIN_LINKS= By.xpath("//a[text()='Log in to FitDay']");
+
+    @Override
+    public boolean isPageOpen() {
+        return isExist(LOGIN_LINKS);
+    }
+
+    public SingUpPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Step("Get error message for password")
+    public String getErrorMessagePassword() {
+        return driver.findElement(ERROR_MESSAGE_PASSWORD).getText();
+    }
+
+    @Step("Get error message for user name")
+    public String getErrorMessageName() {
+        return driver.findElement(ERROR_MESSAGE_NAME).getText();
+    }
+
+    @Step("Get error message for different password")
+    public String getErrorMessageDifferentPassword() {
+        return driver.findElement(ERROR_MESSAGE_DIFFERENT_PASSWORD).getText();
+    }
+
+    @Step("Get error message for invalid password")
+    public String getErrorMessageInvalidEmail() {
+        return driver.findElement(ERROR_MESSAGE_INVALID_EMAIL).getText();
+    }
+}
