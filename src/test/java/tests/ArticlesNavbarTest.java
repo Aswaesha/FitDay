4b922b9b762f72db3ecc4e5846c2d127d118d1f9
Aleksandra <taeyeononni@gmail.com>
2022-02-06 20:08:37 +0300

    add

--- src/test/java/tests/ArticlesNavbarTest.java
+++ src/test/java/tests/ArticlesNavbarTest.java
@@ -11,18 +11,11 @@ public class ArticlesNavbarTest extends BaseTest {
     public void clickArticlesNutritionNavbar() throws InterruptedException {
         loginPage.open()
                 .login(username, password);
-
-      //   homePage.isPageOpen();
-       // navbarPage.isErrorOpen();
-//        if (driver.findElement(By.xpath("//div[@class='step']")).isDisplayed()){
-//            driver.findElement(By.xpath("//div[@class='close']")).click();
-//        }
-        AllureUtils.takeScreenshot(driver);
+        driver.get("https://www.fitday.com/app/home/dashboard#06FEB2022");
         navbarPage.selectMenuOption("ARTICLES", "NUTRITION");
 
-
         assertTrue(articlesNavbarPage.checkoutTitle(), "Nutrition page is not open");
-
+        AllureUtils.takeScreenshot(driver);
     }
 
 //    @Test(description = "Open articles across Navbar with click on articles")
@@ -43,17 +36,12 @@ public class ArticlesNavbarTest extends BaseTest {
     public void clickArticlesFitnessNavbar() throws InterruptedException {
         loginPage.open()
                 .login(username, password);
-        loginPage.login(username, password);
 
-        //driver.findElement(By.xpath("//div[@class='close']")).click();
-        driver.findElement(By.xpath("//div[@id='notice']//div")).click();
-        AllureUtils.takeScreenshot(driver);
         navbarPage.selectMenuOption("ARTICLES", "FITNESS");
-
         navbarPage.isPageOpen();
 
         assertTrue(articlesNavbarPage.checkoutTitle(), "Fitness page is not open");
-
+        AllureUtils.takeScreenshot(driver);
     }
 
 //    @Test(description = "Open home page across navbar")
