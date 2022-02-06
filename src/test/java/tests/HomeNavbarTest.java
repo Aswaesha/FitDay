60227fe2a5cc08a46369854332aa9178caad68c1
Aleksandra <taeyeononni@gmail.com>
2022-02-06 22:40:46 +0300

    add

--- src/test/java/tests/HomeNavbarTest.java
+++ src/test/java/tests/HomeNavbarTest.java
@@ -1,6 +1,9 @@
 package tests;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import utils.AllureUtils;
 
@@ -19,6 +22,23 @@ public class HomeNavbarTest extends BaseTest {
        homeNavbarPage.clickPlusButton();
 
        assertEquals(homeNavbarPage.checkFoodTable(),"Apple, raw","Food not found");
+    }
+        @Test(description = "Open calendar page across navbar")
+    public void clickHomeCalendarNavbar() throws InterruptedException {
+        loginPage.open()
+                .login(username, password);
+
+            Actions action = new Actions(driver);
+            WebElement we = driver.findElement(By.xpath("//a[span[text()='HOME']]"));
+            action.moveToElement(we).build().perform();
+            Thread.sleep(5000);
+
+        driver.findElement(By.xpath("//ul[not(contains(@style, 'none'))]/li//a[text()='CALENDAR']")).click();
+        Thread.sleep(5000);
+
+
+        assertTrue(homeNavbarPage.checkoutCalendarTable(), "Calendar page is not open");
+        AllureUtils.takeScreenshot(driver);
     }
 //    @Test(description = "Open calendar page across navbar")
 //    public void clickHomeCalendarNavbar() {
