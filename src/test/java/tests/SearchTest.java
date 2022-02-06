6819e0d7564af3111fc990ef3a19ef878d4a38dc
Aleksandra <taeyeononni@gmail.com>
2022-02-06 16:44:47 +0300

    add

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,16 +1,16 @@
-package tests;
-
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertTrue;
-
-public class SearchTest extends BaseTest {
-
-    @Test(description = "Search in a search box")
-    public void searchTest() {
-        searchPage.open()
-                .searchInput("apple");
-
-        assertTrue(searchPage.isPageOpen(), "Search page is not open");
-    }
-}
+//package tests;
+//
+//import org.testng.annotations.Test;
+//
+//import static org.testng.Assert.assertTrue;
+//
+//public class SearchTest extends BaseTest {
+//
+//    @Test(description = "Search in a search box")
+//    public void searchTest() {
+//        searchPage.open()
+//                .searchInput("apple");
+//
+//        assertTrue(searchPage.isPageOpen(), "Search page is not open");
+//    }
+//}
