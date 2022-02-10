3a767dcf3f748cef50553993230a98b2f04a9bf3
Aleksandra <taeyeononni@gmail.com>
2022-02-10 23:21:59 +0300

    add

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -35,7 +35,7 @@ public class BaseTest {
 
     @BeforeMethod(alwaysRun = true)
     public void setUp(ITestContext context) {
-        WebDriverManager.chromedriver().browserVersion("98").setup();
+        WebDriverManager.chromedriver().browserVersion("98.0.4758.48").setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
