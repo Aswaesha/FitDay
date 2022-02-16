a04ed4353dfdb568c05de4afb82e960790c68140
Aleksandra <taeyeononni@gmail.com>
2022-02-16 15:50:45 +0300

    add

--- src/test/java/pages/NavbarPage.java
+++ src/test/java/pages/NavbarPage.java
@@ -18,11 +18,14 @@ public class NavbarPage extends BasePage {
 
     @Step("Click on items navbar")
     public void selectMenuOption(String firstMenuOption, String secondMenuOption) {
+        driver.navigate().refresh();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         log.info("hover start");
         Actions action = new Actions(driver);
         WebElement we = driver.findElement(By.xpath(String.format(MAIN_ITEM_NAVBAR, firstMenuOption)));
         action.moveToElement(we).build().perform();
+
+
 
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
